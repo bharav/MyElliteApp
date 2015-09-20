75625ce6cfa6fda24a66b64969a1f1bd1e730faf
bharav <vivek.bharatia@emc.com>
2015-09-20 19:47:19 +0530

    Commit

--- www/app/app.js
+++ www/app/app.js
@@ -29,6 +29,14 @@ angular.module('eliteApp', ['ionic'])
     url: '/home',
     abstract:true,
     templateUrl: 'app/home/home.html'
+  }) 
+  .state('home.leagues', {
+    url: '/leagues',
+    views:{
+      "tab-leagues":{
+      templateUrl: 'app/home/leagues.html'
+      }
+    }
   })
   .state('home.myteams', {
     url: '/myteams',
@@ -38,27 +46,60 @@ angular.module('eliteApp', ['ionic'])
       }
     }
   })
- .state('home.leagues', {
-    url: '/leagues',
-    view:{
-      "tab-leagues":{
-        templateUrl: 'app/home/leagues.html'    
-      }
-    }
-  })  
-  .state('app.teams', {
-    url: '/app/teams',
-    templateUrl: 'app/teams/teams.html'
-  })
-  .state('app.teams-detail', {
-    url: '/app/teams/:id',
-    templateUrl: 'app/teams/teams-detail.html'
-  })
   .state('app', {
     url: '/app',
+    abstract:true,
     templateUrl: 'app/layout/menu-layout.html'
+  })
+    .state('app.teams', {
+    url: '/teams',
+    views:{
+      'mainContent':{
+      templateUrl: "app/teams/teams.html"
+      }
+    }
+  })
+    .state('app.teams-detail', {
+    url: '/teams/:id',
+    views:{
+      'mainContent':{
+      templateUrl: "app/teams/teams-detail.html"
+      }
+    }
+  })
+    .state('app.games', {
+    url: '/games',
+    views:{
+      'mainContent':{
+      templateUrl: "app/games/games.html"
+      }
+    }
+  })
+  .state('app.standings', {
+    url: '/standings',
+    views:{
+      'mainContent':{
+      templateUrl: "app/standings/standings.html"
+      }
+    }
+  })
+   .state('app.locations', {
+    url: '/locations',
+    views:{
+      'mainContent':{
+      templateUrl: "app/locations/locations.html"
+      }
+    }
+  })
+  .state('app.rules', {
+    url: '/rules',
+    views:{
+      'mainContent':{
+      templateUrl: "app/rules/rules.html"
+      }
+    }
   });
   // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/home/leagues');
+  $urlRouterProvider.otherwise('/app/teams');
 
 });
