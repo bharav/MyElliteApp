9cef09e8f667a5bfb1ccab3a5111f1a009ad9cd4
bharav <vivek.bharatia@emc.com>
2015-09-20 22:26:31 +0530

    commit

--- www/app/home/LeaguesCtrl.js
+++ www/app/home/LeaguesCtrl.js
@@ -1,11 +1,15 @@
 (
 	function(){
 		'use strict'
-		angular.module('eliteApp').controller('LeaguesCtrl',["eliteApi",LeaguesCtrl])
-		function LeaguesCtrl(eliteApi){
+		angular.module('eliteApp').controller('LeaguesCtrl',['$state','eliteApi',LeaguesCtrl])
+		function LeaguesCtrl($state,eliteApi){
 			var vm = this;
 			var leagues=eliteApi.getLeagues();
 			vm.leagues=leagues;
+			vm.selectLeague=function(leagueId){
+				//TODO
+				$state.go("app.teams");
+			};
 			
 		}
 	}
